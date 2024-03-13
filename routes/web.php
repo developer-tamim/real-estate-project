0ef840a6d8e9bf6d644d9e28b60c346776c21594
developer-tamim <tawsiftamim24@gmail.com>
2024-03-13 13:08:32 +0600

    admin dashboard and authentication

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,9 @@ require __DIR__.'/auth.php';
 //Admin
 Route::middleware(['auth','role:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 });
+Route::get('/admin/login', [AdminController::class, 'Adminlogin'])->name('admin.login');
 
 //Agent
 Route::middleware(['auth','role:agent'])->group(function(){
