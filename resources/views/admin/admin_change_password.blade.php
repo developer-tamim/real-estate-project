4f39ae6b90eee49d167cce86b81a27ffe1b1dad3
developer-tamim <tawsiftamim24@gmail.com>
2024-03-14 13:20:06 +0600

    admin profile and change passeord

--- resources/views/admin/admin_change_password.blade.php
+++ resources/views/admin/admin_change_password.blade.php
@@ -55,45 +55,32 @@
                     <div class="card">
                         <div class="card-body">
 
-                            <h6 class="card-title">Update Admin Profile</h6>
+                            <h6 class="card-title">Admin Change Password</h6>
 
-                            <form class="forms-sample" method="POST" action="{{ route('admin.profile.store') }}" enctype="multipart/form-data">
+                            <form class="forms-sample" method="POST" action="{{ route('admin.update.password') }}">
                                 @csrf
+
                                 <div class="mb-3">
-                                    <label for="username" class="form-label">Username</label>
-                                    <input type="text" name="username" class="form-control" id="username" autocomplete="off"
-                                        value="{{ $profileData->username }}">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="name" class="form-label">Name</label>
-                                    <input type="text" name="name" class="form-control" id="name" value="{{ $profileData->name }}">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="email" class="form-label">Email</label>
-                                    <input type="email" name="email" class="form-control" id="email"
-                                        autocomplete="off" value="{{ $profileData->email }}">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="phone" class="form-label">Phone</label>
-                                    <input type="number" name="phone" class="form-control" id="phone"
-                                        autocomplete="off" value="{{ $profileData->phone }}">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="address" class="form-label">Address</label>
-                                    <input type="text" name="address" class="form-control" id="address"
-                                        autocomplete="off" value="{{ $profileData->address }}">
+                                    <label for="name" class="form-label">Old Password</label>
+                                    <input type="password" name="old_password" class="form-control @error('old_password') is-invalid @enderror" id="old_password" autocomplete="off">
+                                    @error('old_password')
+                                        <span class="text-danger">{{ message }}</span>
+                                    @enderror
                                 </div>
 
                                 <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">Photo</label>
-                                    <input class="form-control" type="file" name="photo" id="image">
+                                    <label for="name" class="form-label">New Password</label>
+                                    <input type="password" name="new_password" class="form-control @error('new_password') is-invalid @enderror" id="new_password" autocomplete="off">
+                                    @error('new_password')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
 
                                 <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label"></label>
-                                    <img id="showImage" class="wd-80 rounded-circle" src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo):url('upload/no_image.jpg') }}" alt="profile">
-                                </div>
+                                    <label for="name" class="form-label">Confirm New Password</label>
+                                    <input type="password" name="new_password_confirmation" class="form-control" id="new_password_confirmation" autocomplete="off">
 
+                                </div>
 
                                 <button type="submit" class="btn btn-primary me-2">Save Changes</button>
 
@@ -107,17 +94,5 @@
         </div>
     </div>
 
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $('#image').change(function(e){
-                var reader = new FileReader();
-                reader.onload = function(e){
-                    $('#showImage').attr('src',e.target.result);
-                }
-                reader.readAsDataURL(e.target.files[0]);
-            });
-        });
 
-
-    </script>
 @endsection
--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -1,13 +1,4 @@
 <!DOCTYPE html>
-<!--
-Template Name: NobleUI - HTML Bootstrap 5 Admin Dashboard Template
-Author: NobleUI
-Website: https://www.nobleui.com
-Portfolio: https://themeforest.net/user/nobleui/portfolio
-Contact: nobleui123@gmail.com
-Purchase: https://1.envato.market/nobleui_admin
-License: For each use you must have a valid license purchased only from above link in order to legally use the theme for your project.
--->
 <html lang="en">
 
 <head>
@@ -45,6 +36,8 @@ License: For each use you must have a valid license purchased only from above li
     <!-- End layout styles -->
 
     <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.png') }}" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+
 </head>
 
 <body>
@@ -87,6 +80,31 @@ License: For each use you must have a valid license purchased only from above li
     <!-- Custom js for this page -->
     <script src="{{ asset('backend/assets/js/dashboard-dark.js') }}"></script>
     <!-- End custom js for this page -->
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+    <script>
+     @if(Session::has('message'))
+     var type = "{{ Session::get('alert-type','info') }}"
+     switch(type){
+        case 'info':
+        toastr.info(" {{ Session::get('message') }} ");
+        break;
+
+        case 'success':
+        toastr.success(" {{ Session::get('message') }} ");
+        break;
+
+        case 'warning':
+        toastr.warning(" {{ Session::get('message') }} ");
+        break;
+
+        case 'error':
+        toastr.error(" {{ Session::get('message') }} ");
+        break;
+     }
+     @endif
+    </script>
 
 </body>
 
