df6781fc23ebfc7f99d88187fe3381cf29f320e1
developer-tamim <tawsiftamim24@gmail.com>
2024-03-18 16:01:02 +0600

    add property type

--- app/Http/Controllers/Backend/PropertyTypeController.php
+++ app/Http/Controllers/Backend/PropertyTypeController.php
@@ -12,4 +12,28 @@ class PropertyTypeController extends Controller
         $type = PropertyType::latest()->get();
         return view('backend.type.all_type', compact('type'));
     }
+
+
+    public function AddType(){
+        return view('backend.type.add_type');
+    }
+    public function StoreType(Request $request){
+        $request->validate([
+            'type_name' => 'required|unique:property_types|max:200',
+            'type_icon' => 'required',
+        ]);
+
+        PropertyType::insert([
+            'type_name' => $request->type_name,
+            'type_icon' => $request->type_icon,
+        ]);
+
+        $notification = array(
+            'message' => 'Property Type Created Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.type')->with($notification);
+
+    }
 }
